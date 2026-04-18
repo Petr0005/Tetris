254607da0fc19f7a82d335e924933a74866308a9
Petr Barkhatov <qwertywasd0002@mail.ru>
2026-04-18 12:38:46 +0700

    display create window

---
+++ src/GamePanel.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+
+    public GamePanel() {
+
+        // Panel Settings
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.black);
+        this.setLayout(null);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        
+        JFrame window = new JFrame("Simple Tetris");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        // Add GamePanel to the window
+        GamePanel gp = new GamePanel();
+        window.add(gp);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
